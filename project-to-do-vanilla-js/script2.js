3f9dbe17918fecc3a49c0f5b94fb8a50abad22a0
farazhafeez <faraz.hafiz@gmail.com>
2020-09-10 20:20:28 +0200

    made changes

--- project-to-do-vanilla-js/script2.js
+++ project-to-do-vanilla-js/script2.js
@@ -7,24 +7,23 @@ var todoList = document.getElementById("todo-list")
 // should be able to delete items 
 // should store items to local storage
 
-
+// 2 
 function showtodos() {
     // clean to do list
-    todoList.innerHTML = ""
+    document.getElementById("todo-list").innerHTML = ""
     for (var i = 0; i < todos.length; i++) {
         var todo = document.createElement("P")
         todo.innerText = todos[i]
-        todoList.appendChild(todo)
+        document.getElementById("todo-list").appendChild(todo)
     }
 
 
 }
-
+// 1 
 form.addEventListener("submit", function (event) {                              // event     
     event.preventDefault()   //to avoid page refresh on form submit
-    var todoText = input.value
+    var todoText = document.getElementById("todo-item").value
     todos.push(todoText)
     showtodos()
 })
-
 
